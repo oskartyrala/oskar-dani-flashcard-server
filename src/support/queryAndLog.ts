2d61cd338b631159e5b0ab196ae56c0369967692
oskartyrala <oskartyrala@protonmail.com>
2023-08-30 12:23:09 +0200

    Implement query end console log

--- src/support/queryAndLog.ts
+++ src/support/queryAndLog.ts
@@ -1,3 +1,4 @@
+import { performance } from "perf_hooks";
 import { Client, QueryResult } from "pg";
 
 export default async function queryAndLog(
@@ -6,11 +7,17 @@ export default async function queryAndLog(
     sql: string,
     params: string[] = []
 ): Promise<QueryResult> {
-    console.log(
-        `SQL START qNum: ${qNum
-            .toString()
-            .padStart(4, "0")} sql: ${sql} params: ${params}`
-    );
+    const paddedNum = qNum.toString().padStart(4, "0");
+    console.log(`SQL START qNum: ${paddedNum} sql: ${sql} params:`, params);
+    const startTime = performance.now();
     const response = await client.query(sql, params);
+    const endTime = performance.now();
+    const elapsedTime = (endTime - startTime).toFixed(3);
+    const paddedTime = elapsedTime.toString().padStart(10, " ");
+    const paddedRowCount = response.rowCount.toString().padStart(5, " ");
+    console.log(
+        `SQL END   qNum: ${paddedNum} time: ${paddedTime}ms rowCount: ${paddedRowCount} sql: ${sql} params:`,
+        params
+    );
     return response;
 }
